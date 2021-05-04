a6e61361965544e2242ee81221460269f1702701
jlovgrendotcom <jmlovgren@gmail.com>
2021-05-03 20:55:27 -0700

    Adding completed HW-4 project

--- main.js
+++ main.js
@@ -1,31 +1,25 @@
-let inputname = document.getElementById("2moviename").value;
- 
-let inputrating = document.getElementById("2rating").value;
 
-const inputmovie = new movieobj(inputname, inputrating);
-
-let movieinfo = new Array();
-
+let movieinfo = new Array ();
 
 function formSubmitEvent(){
+  let inputname = document.getElementById("2moviename").value;
+  let inputrating = document.getElementById("2rating").value;
+  const inputmovie = new Movie(inputname, inputrating);
+  
   console.log(inputmovie); 
-  vaiLdate(inputmovie)
-    if (vaiLdate(inputmovie) == false){
-  alert("Movie name must be filled out and the rating must be between 1 - 5.")
+    if (validate(inputmovie) == false){
+  alert("Enter a movie name and a rating between 1-5.")
     }
     else{
       movieinfo.push(inputmovie)
     }
 }
-   
-
+  
 function showMovieEvent(){
-  let xs;
-  let displaymovie = [];
-  let ps = getAll();
+  let xs = "";
   for (i = 0; i<movieinfo.length; i++){
-    displaymovie.push(ps)
-  xs = xs+ displaymovie[i]+"<br>";
+    let ps = getAll(movieinfo[i]);
+    xs += ps + "<br>";
   }
   document.getElementById('renderList').innerHTML = xs;
   
--- movie.js
+++ movie.js
@@ -1,29 +1,27 @@
-let movieobj = function(mname, mrating) {
+
+let Movie = function (mname, mrating) {
     this.name = mname;
     this.rate = mrating;
-    this.msg = function(){
-      return (this.name + " "+"has a rating of"+""+this.rate);
-    }
   }
 
-  function vaiLdate() {
-    if (this.name  == "" || this.name == null) {
+  function validate(inputmovie) {
+    console.log(inputmovie.name, inputmovie.rate);
+    if (inputmovie.name  === "" || inputmovie.name === null) {
     return false; 
     }
-    else if (this.rate >5){
-         return (false);
+    else if (inputmovie.rate >5){
+         return false;
     }
-    else if (this.rate <1){
-        return (false);
+    else if (inputmovie.rate <1){
+        return false;
     }
     else {
        console.log("i am here on movie.js")
-       return (true);
+       return true;
     }
   }
-  function getAll(){
-    return this.msg;
+  function getAll(inputmovie){
+    return inputmovie.name + " has a rating of " + inputmovie.rate;
   }
-
 
 
